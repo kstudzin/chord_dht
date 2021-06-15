d06cdcea88753ca23202c9c80e6fd27fca3865e6
Kate Studzinski <kstudzinski@acm.org>
2021-06-15 15:51:40 -0400

    Add tests for naive routing

--- chord/node.py
+++ chord/node.py
@@ -1,3 +1,10 @@
+import statistics
+
+from sortedcontainers import SortedDict
+
+from util import generate_keys
+from hash import hash_value
+
 
 class Node:
 
@@ -31,3 +38,62 @@ class Node:
                 return self.successor, hops
             else:
                 return self.successor.find_successor(digest, hops + 1)
+
+
+def build_nodes(num_nodes):
+    node_name_fmt = "node_{id}"
+    nodes = SortedDict()
+
+    for i in range(num_nodes):
+        name = node_name_fmt.format(id=str(i))
+        digest = hash_value(name)
+        nodes[digest] = Node(name, digest)
+
+    # Use a separate iterator over the nodes to set the successors
+    successors_list = list(nodes.values())
+    successors = iter(successors_list)
+
+    # Last node successor needs to be set to the first node
+    first_node = next(successors)
+    last_node = successors_list[-1]
+    last_node.set_successor(first_node)
+
+    # We have already set successor on the last node. Now set successor
+    # on n-1 earlier nodes
+    for node_digest in nodes.keys()[:-1]:
+        next_node = next(successors)
+        nodes[node_digest].set_successor(next_node)
+
+    return nodes
+
+
+def run_experiment(num_nodes, num_keys):
+    nodes = build_nodes(num_nodes)
+    keys = generate_keys(num_keys)
+
+    hops_tracker = []
+    starting_node = nodes.values()[0]
+    for key in keys:
+        node, hops = starting_node.find_successor(hash_value(key), 0)
+        hops_tracker.append(hops)
+
+    return statistics.mean(hops_tracker)
+
+
+def main():
+    num_nodes = 50
+    num_keys = 100
+    avg_hops = run_experiment(num_nodes, num_keys)
+    print(f"Average hops with {num_nodes} nodes is {avg_hops}")
+
+    num_nodes = 100
+    avg_hops = run_experiment(num_nodes, num_keys)
+    print(f"Average hops with {num_nodes} nodes is {avg_hops}")
+
+    # The number of hops to find a key using naive routing should be
+    # proportional to the number of nodes in the network. Specifically
+    # it should be close to n/2 which is what we see here
+
+
+if __name__ == "__main__":
+    main()

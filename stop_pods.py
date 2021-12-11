f18c1b1e21a3a5de2bc105a55ddcde30f5bae96f
Kate Studzinski <kstudzinski@acm.org>
2021-12-11 17:25:30 -0500

    fix random pod selection

--- stop_pods.py
+++ stop_pods.py
@@ -11,15 +11,14 @@ print(f'Stopping a pod after an average of {seconds} seconds')
 kubernetes.config.load_kube_config()
 
 v1 = kubernetes.client.CoreV1Api()
-pods = v1.list_pod_for_all_namespaces(label_selector='app=chord')
+pods = v1.list_pod_for_all_namespaces(label_selector='app=chord').items()
 
-pod_ids = pods.keys()
 while True:
-    pod_idx = random.randrange(len(pod_ids))
-    pod_key = pod_ids[pod_idx]
-    pod = pods[pod_key]
+    pod_idx = random.randrange(len(pods))
+    pod = pods[pod_idx]
     deleted = v1.delete_namespaced_pod(pod.metadata.name, 'chord')
     print(f'Deleted pod {pod.metadata.name}')
+
     sleep_time = random.gauss(seconds, 3)
     time.sleep(sleep_time)
 
